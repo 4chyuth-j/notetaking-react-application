547c517b3917f23f4e2c05f1f620e1fb4f005dc4
4chyuth-j <achyuthj2002@gmail.com>
2025-10-24 23:29:37 +0530

    setup of middleware and ratelimit using redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,40 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
-
+import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
 
+const PORT = process.env.PORT || 5001;
+
+
+
 const app = express();
-
-connectDB();
-
 
 //middleware
 //Before handling any routes, if the request has a JSON body, automatically read it and convert it into a JavaScript object.
 app.use(express.json());
 
-app.use("/api/notes",notesRoutes);
+app.use(rateLimiter);
 
-const PORT = process.env.PORT || 5001;
+//simple coustom middleware
+// app.use((req,res,next)=>{
+//   console.log(`Req method is:-${req.method} & Req url is:-${req.url}`);
+//   next();
+// });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running at: http://localhost:${PORT}`); 
+app.use("/api/notes", notesRoutes);
+
+
+//only run the application after connecting to the database
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Server running at: http://localhost:${PORT}`);
+  });
 });
+
+
+
+
 
 
 
