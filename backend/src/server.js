4fbdbfdcd782b05203683254c90c70d747ad9b02
4chyuth-j <achyuthj2002@gmail.com>
2025-10-24 12:20:19 +0530

    data base and .env configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from 'express';
-import notesRoutes from './routes/notesRoutes.js'
+import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
 
 const app = express();
+
+connectDB();
 
 
 app.use("/api/notes",notesRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running at: http://localhost:${PORT}`);
+  
 });
+
+
+
+
